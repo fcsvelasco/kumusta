f9947f218e5a937611db94fe01444ff1d70f8502
fcsvelasco <119574621+fcsvelasco@users.noreply.github.com>
2023-02-09 11:15:04 +0800

    removed unused import

--- lib/widgets/current_checker/observations_list.dart
+++ lib/widgets/current_checker/observations_list.dart
@@ -1,7 +1,6 @@
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:worksampler/styling/app_theme_data.dart';
 
 import '../../models/observation.dart';
 
